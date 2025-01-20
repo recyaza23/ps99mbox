6b066f63017ef1dc1fb00ecbc4a311fcf77f4369
recyaza23 <kusprama@gmail.com>
2025-01-20 20:05:54 +0800

    Update exodusps99plaza.lua

--- exodusps99plaza.lua
+++ exodusps99plaza.lua
@@ -14,23 +14,22 @@ getgenv().Settings = {
             ["All Huges"] = {Price = "15m", DetectManipulation = true, AllTypes = true},
         },
         ["Switch Servers"] = {Active = true, SecondDelay = 7, OnlyPRO = false},
-        ["Webhook"] = {Active = true, URL = "https://discord.com/api/webhooks/1249534633331261540/I9Fr_1feUgpp7GuAqhdh90Dypaezdu-NYa1emcPc0wFqH3uadwQcxnhNlCF0_Z0TJ0kM"},
+        ["Webhook"] = {Active = true, URL = ""},
         ["Kill Switch"] = {
             ["Limits Reached"] = false,
             ["Diamonds Hit: 25m"] = false,
             ["60 Minutes Timer"] = false,
-            ["^^^ Switch To Selling"] = true,
+            ["^^^ Switch To Selling"] = false,
         },
     },
 
     Seller = {
         Active = true,
         ["Items"] = {
-            ["All Huges"] = {Price = "-4%", AllTypes = true},
-            ["Shiny Huge Player Fox"] = {Price = "200m"},
+            ["All Huges"] = {Price = "-3%", AllTypes = true},
         },
         ["Switch Servers"] = {Active = true, MinuteDelay = 20, OnlyPRO = false},
-        ["Webhook"] = {Active = true, URL = "https://discord.com/api/webhooks/1249534633331261540/I9Fr_1feUgpp7GuAqhdh90Dypaezdu-NYa1emcPc0wFqH3uadwQcxnhNlCF0_Z0TJ0kM"},
+        ["Webhook"] = {Active = false, URL = "https://discord.com/api/webhooks/1249534633331261540/I9Fr_1feUgpp7GuAqhdh90Dypaezdu-NYa1emcPc0wFqH3uadwQcxnhNlCF0_Z0TJ0kM"},
         ["Kill Switch"] = {
             ["Booth Runout"] = false,
             ["Diamonds Hit: 1b"] = false,
@@ -39,5 +38,7 @@ getgenv().Settings = {
         },
         ["Diamonds Sendout"] = {Active = true, Username = "recyaza", Amount = "250m"},
     },
+
+    [[ Thank you for using System Exodus <3! ]]
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b58d9c50c5ea74f255af40a8f735773.lua"))()
