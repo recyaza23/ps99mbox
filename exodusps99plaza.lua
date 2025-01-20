7297aae4a2bff5f9cdf41f93a5a6473ce732dc56
recyaza23 <kusprama@gmail.com>
2025-01-20 20:17:51 +0800

    Update exodusps99plaza.lua

--- exodusps99plaza.lua
+++ exodusps99plaza.lua
@@ -1,44 +1,45 @@
-script_key = "taqBOUrOHpHmEIvWfzyLNZFJCooAYQDl";
+script_key = "taqBOUrOHpHmEIvWfzyLNZFJCooAYQDl"
 getgenv().Settings = {
     Sniper = {
         Active = false,
-        ["Items"] = {
-            SearchTerminal = {
-                [[ Search Items via Trading Terminal, Custom Keywords NOT supported here. ]],
-                ["Nice Egg"] = {Price = "5%", InventoryLimit = 355},
-                ["Huge Night Terror Cat"] = {Price = "50%", UseCosmicValues = true},
-            },
-            ["All Class: Misc"] = {Price = 5},
-            ["All Rarity: Celestial"] = {Price = "50%"},
-            ["Shiny Rainbow Broomstick Cat"] = {Price = "+2%"},
-            ["All Huges"] = {Price = "15m", DetectManipulation = true, AllTypes = true},
-        },
-        ["Switch Servers"] = {Active = true, SecondDelay = 7, OnlyPRO = false},
-        ["Webhook"] = {Active = true, URL = ""},
-        ["Kill Switch"] = {
+        StopParams = {
             ["Limits Reached"] = false,
-            ["Diamonds Hit: 25m"] = false,
-            ["60 Minutes Timer"] = false,
-            ["^^^ Switch To Selling"] = false,
+            ["Diamonds Hit: 250k"] = true,
+            ["60 Minutes"] = false,
+            ["Switch To Selling"] = true,
         },
     },
 
     Seller = {
         Active = true,
-        ["Items"] = {
-            ["All Huges"] = {Price = "-3%", AllTypes = true},
+        Items = {     
+            ["All Huges"] = {Class = "Pet", Price = "-3%", AllTypes = true},
+            -- ["Millionaire Bucks"] = {Class = "Misc", Price = "-8%"},
         },
-        ["Switch Servers"] = {Active = true, MinuteDelay = 20, OnlyPRO = false},
-        ["Webhook"] = {Active = false, URL = "https://discord.com/api/webhooks/1249534633331261540/I9Fr_1feUgpp7GuAqhdh90Dypaezdu-NYa1emcPc0wFqH3uadwQcxnhNlCF0_Z0TJ0kM"},
-        ["Kill Switch"] = {
-            ["Booth Runout"] = false,
-            ["Diamonds Hit: 1b"] = false,
-            ["60 Minutes Timer"] = false,
-            ["^^^ Switch To Sniping"] = false,
+        Serverhop = {
+            ["Switch Servers"] = true,
+            ["Teleport Delay (m)"] = 35,
+            ["Add Pro Plaza Lobbies"] = false,
         },
-        ["Diamonds Sendout"] = {Active = true, Username = "recyaza", Amount = "250m"},
+        Webhook = {
+            ["URL"] = "https://discord.com/api/webhooks/1249534633331261540/I9Fr_1feUgpp7GuAqhdh90Dypaezdu-NYa1emcPc0wFqH3uadwQcxnhNlCF0_Z0TJ0kM",
+            ["Send Embeds"] = true,
+            ["Remove Username"] = false,
+        },
+        StopParams = {
+            ["Item Runout"] = false,
+            ["Diamonds Hit: 20b"] = false,
+            ["60 Minutes"] = false,
+            ["Switch To Sniping"] = false,
+        },
+        Other = {
+            ["Auto Accept Mail"] = true,
+            ["Always Try Adding Listings"] = true,
+            ["Never Join Friendslist"] = false,
+        },
     },
-
+    
     [[ Thank you for using System Exodus <3! ]]
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b58d9c50c5ea74f255af40a8f735773.lua"))()
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Mailbox: Claim All"):InvokeServer()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/60a293774110e918789cddc0e20be048.lua"))()
