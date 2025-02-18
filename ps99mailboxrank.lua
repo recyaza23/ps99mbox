27d936df16d91d90cd5376698e302d3307cae218
recyaza23 <kusprama@gmail.com>
2025-02-18 13:08:07 +0800

    Update ps99mailboxrank.lua

--- ps99mailboxrank.lua
+++ ps99mailboxrank.lua
@@ -1,53 +1,398 @@
+-- --60m Gems,75 pet,20 pot coin,20 pot damage,20 pot treasure,400 pot coin4,speedpot
+-- --1 super lightning,1 coin,1critical,1strong,500 comet,500 coin jar,500 rainbow fruit,400 ball,1000 diamond flag
+-- gems= "ac2c62f023e6494ea8a6624c49a2bdb0",Currency
+-- coin7pot="272dbc5a55424fe3a9c3c21209e4a2f8",Potion
+-- damage7pot="b940726bdbee412eb55dd483f5669834",Potion
+-- treasure7pot="22ca52843a0a413899a9036fe31885fb",Potion
+-- coin4pot="69c986d52e7a4f539863aac8e75cabe8",Potion
+-- speed3pot="629eed11b22c4db69b2c183bde4097c8",Potion
+-- superlightning="2efa3f8f01eb4ad3800acb48ce42272a",Enchant
+-- coin7enc="87d59437bb514ffb95677ea969fca8e0",Enchant
+-- crit7enc="9454e7cd015c414d8fd0d0d97517e977",Enchant
+-- strong7enc="baa8eb8e9b1448b086912be9f2d00ede",Enchant
+-- comet="66eb23e8d2cb4464b1045d05e8249e3f",Misc
+-- coinjar="8f42d1247215400a86bd7449e2c7142d",Misc
+-- toyball="1e9c953e07724043865b462f9ff92e22",Misc
+-- diamondflag="8c8ef0e0ef664e3d99b7e27df6d707bb",Misc
+-- rainbowfruit="90cf2192290046c8a0ae1031814ff671",Fruit
+
+-- Pinata= "3003b07bc6e14149a44e2240e27000ff",Misc
+-- Luckyblock="a859ae3844a74c6291efaf0bf3fc71a8",Misc
+-- Luckyenc="50ef07cdaf98421981b09f9b6ecc94b9",Enchant
+-- SuperiorChest="92b1f16f144e47339b05fc04010d32ca",Enchant
+-- Chestspell="5b79942ee7a2446eb64aaeae09905054",Ultimate
+-- kawaiipet= "2ac619ec5d454ac4b27fc384dce5a1f3",Pet
+-- ChefMonkey= "8981e67c27dc41e581fcfd809f19db80",Pet
+-- abstract agony="abb683ea91c142a08ee9747e23d8952f",Pet
+--gorgonpet ="b34eae172b2b4a0fa25c55060e577668",Pet
+
 local usernamesString = [[
-Ggdeacouao
-Gkeaeipiew
-Auoetpqqk
-Eucmvuetu
-Adxwbpooueb
-Uteyvoauu
-Iilgdffyxim
-Jrgyburcko
-Imiwoxxjz
-Ixoeeqicooj
-Yacbylawoy
-Upaamxzeuer
-Eedswbaye
-Vutcalgua
-Bicihbbug
-Ybncorreafk
-Uzzzxujuo
-Otjfeumnewo
-Wimznoigmp
-Kanrilyir
-Qakroiaov
-Pguulserl
-Ibogjweux
-Cqmkjyuso
-Yubrpokkf
-Vdraryoeaj
-Qejeegcyw
-Uofxteuuoi
-Zomaxaauzjs
-Ffururaieea
-Pggapztuil
-Soknojkoudk
-Jsrduenigke
-Hpnudurte
-Eiisoxiawp
-Dkukvoeraqa
-Aiyttevam
-Ojcjptaoo
-Gffouxwae
-Enapexcub
-Evisenbpaxa
-Zfasmewldok
-Youeaznshux
-Afaaqjneo
-Lkaiaerwe
-Panjaeqfrba
-Eiwdfecxui
-Ygewiuxhe
-Dmoatuepavi
+Ultra38SpiritSA
+Golden10KeeperSA
+Dragon41GuardSA
+Cyber83WalkerSA
+Shadow65HeroSA
+Moon86ShieldSA
+Frost14RunnerSA
+Sun98WalkerSA
+Ninja87DragonSA
+Crystal89MasterSA
+Epic80DragonSA
+Ninja21HunterSA
+Cool31ShieldSA
+Ultra59PlayerSA
+Super21WarriorSA
+Golden98PrinceSA
+Epic88QueenSA
+Ookiilnir
+Tddaiacraa
+Lakuqtefb
+Fpewpvpedx
+Vztirubgxiq
+Trkyjuannob
+Iaivogedvb
+Lxkazakensv
+Yfyvsikava
+Avbeuczzcd
+Fpvikmalzi
+Myooucxjae
+Gftcqfjuixa
+Oimwsenaoi
+Dawxuaukyv
+Wtyutivwirm
+Iqvxrmobm
+VanishingDragon17
+awepekay
+rezfarm04
+dumbogaming379
+bCoralz
+jukretros
+Ditzhe
+hoamssp
+faidyv
+bot_life23
+AlmKirito
+tm1r39x1
+39171se3
+sunwalker226
+jancccoks
+jajasasoue
+ransens3
+Ntshonsvm
+Baamhladng
+Cettlkmsq
+Shmfyslctlt
+Rcnautrhra
+Xrhihhmgm
+Coobgmzqiq
+HappyAscent2731
+AetherCharlie6920
+MegaQueen6216
+StarQueen2600
+tchnpr
+Thunder45RiderSA
+tpkgaz
+abangkral
+bankuez
+nbenges
+ganbazg
+Paucuaehiio
+Qolijrxhau
+Yijezngsww
+Epbmueiar
+Utqnxsgbiee
+Oguszayxa
+Lulreuaci
+Rojrniuhq
+Elzicpvuvh
+Uoezceovqf
+Aaetlcpjy
+Ohiulmhii
+Ddeunafie
+Kotoqqumf
+Sxmkbirazgu
+Zlbeejdue
+Aisovbxzp
+Nwukepvfe
+Eiaipixasu
+Nueoszuklo
+Bhczkokexa
+Bochuuyiqvd
+Galobmjzqp
+Iiokvzijgp
+Joyrzioxob
+Xvfdcaeopu
+Wtnivlmip
+Einfedaeyd
+Leqeujbfxut
+Gkanwolxa
+Hyxizrjeme
+Plddiiore
+Jkuwfkaquni
+Mfeomoqol
+Ouluelegoi
+Fxhkokfafg
+Htettxuwoq
+Bvlkobtyzz
+Jxiuiqoolic
+Fbhbolyci
+Eosicuufsn
+Ilapmaizgb
+Ouebqtyms
+Tesnynjagia
+Uutjzbocsin
+Jatrsepcceg
+Rrwlxiedqo
+Cwoguuacuzs
+Ahcoofykqy
+Imdhkucnpm
+Wqmaebemm
+Donajlniuxq
+Cmrqxocue
+Qdpxiesnxe
+Itoyunwpk
+Nnewinoopi
+Omimixqavda
+Dinkurolauq
+Fajiafsuru
+Lanismhii
+Muefahchrka
+Veiiesqlxfa
+Oumqywsun
+Asquaotsm
+Ebreiwoclvd
+Avasvquaye
+Uiutxmjofs
+Rlagvrqex
+Ilddulmii
+Oiepiaeiaqb
+Aoucbzcsi
+Mtrdmajry
+Fmuiyeinj
+Toaaqonooi
+Oyjjxogjuy
+Zjinolbyau
+Riweiyihili
+Ejgeinvtyya
+Bufiunneur
+Asiqjeumltd
+Uuutzqleek
+Uzozhxjpo
+Eounsumpeu
+Lbxujviee
+Cnonywnyld
+Umfzalkdds
+Goriyqpuloa
+Eucljovqg
+Poebznaaxe
+Idaohfunjch
+Ezoimroirij
+Oebefpztoai
+Odkrprkbugo
+Koaobeiryfw
+Emudipwde
+Namaaqtoi
+Ohiafjboaw
+Oebexelgi
+Olzvtcywyau
+Mcdnwmioixe
+Ujvmuheuiak
+Gfnknayoq
+Nyaeumvzoe
+Rmoxkusfooh
+Eiqocsyuezd
+Biwuywtayu
+Idmlubukid
+Vsrwpoaow
+Jifriediize
+Ycpmkyjseu
+Rlmmeuauaem
+Wrmpqazuzkw
+Xuiubtaipe
+Jjonendbayq
+Yrszijkie
+Gcejwuoibg
+Taqsjicki
+Hqehdfijja
+Oqpaafunwwo
+Uuvuuoxbea
+Ualrzfzulo
+Vutxvborh
+Rhaoppkutg
+Gmpjyouujj
+Naoialueet
+Ewavlozhagd
+Upjuuoeguh
+Iaxofzeritw
+Pkazpiiua
+Ojaceamdqj
+Oabawoiohu
+Auiauwdfof
+Insxnpeyod
+Asegvcuepib
+Akkblecnuqa
+Iifvuowlc
+Qpbxaoqhb
+Aisiortfab
+Aoodjidewuo
+Eoufuoiie
+Fhbqllekita
+Itmpknuzxav
+Reuajaoas
+Bngeixaaim
+Eaayrjlryql
+Lzdosaoxy
+Utcwoekhel
+Oyizkauwf
+Weizuhwiw
+Iacamnuheu
+Pifqdfyld
+Bazsuyyaie
+Uylapawudl
+Mauajseul
+Yyopacoyylm
+Uxiezaiecej
+Togexwjvkp
+Bcnffioiko
+Ediclubhd
+Qpmrmhaaal
+Ahocxacbg
+Rqvohpjldwc
+Byiydlusa
+Enicxohreve
+Knbraalkldt
+Bysgadicyp
+Utxwoctcuke
+Afohcwaxne
+Zijuqzqutxu
+Oyeejaleaq
+Uoejobqjooh
+Foecutulha
+Ugdaapbdr
+Oeatinecrer
+Ymbdomupjij
+Pelqfmapail
+Dzfuuyeeuq
+Romoioitado
+Uuiuvmznkti
+Fsalisonytj
+Alassnjiaxx
+Iiekittpia
+Opuhhaffjv
+Obbekdaeqh
+Nhgbmfprt
+Rosbtenda
+Skgcpfzume
+Jiicebheney
+Qatpiaiiujs
+Phvfebuoe
+Aoeagyoog
+Uueuwieke
+Eifroykivub
+Sersthewu
+Fbalstsip
+Aruewinynu
+Iylqfgeswi
+Gpaqeabuvj
+Xejvuwcfwb
+Dryjzsveeca
+Ucugavrei
+Nzolhzldaf
+Zjcibuailur
+Gatismefyd
+Eokiisekkv
+Railogivol
+Ytuaebuoeiw
+Zvbfsqueu
+Rakcuokpu
+Ctgiuuouyos
+Bblhwvgvci
+Bkpmgyuluu
+Igxuduzoef
+Uurqthxdkq
+Eoautuoxehs
+Puunoebac
+Oaalxgvigi
+Vyixmqyotno
+Aiofariuu
+Oiuskeooyvm
+Mbgoqzxqrs
+Pecfgyauiic
+Peydinyrwf
+Iadhceuoj
+Ikjhpuqii
+Tonowibynca
+Nfwuqsimu
+Easqiukou
+Iinyyrhsa
+Bwihitaune
+Eulvgivap
+Ogmmnxzevey
+Iuixcrzeio
+Suychzopvts
+Ihjaqmeiuju
+Clicqislau
+Cdgyewvjme
+Yiroqqoeg
+Aenxuzkfnxw
+Ozalhivaua
+Ouoissewtt
+Ndaijcpgboe
+Ljocxogdlku
+Oarmqcemqmw
+Rizfzqwlik
+Upeeeikxyue
+Iyrhecrni
+Cvkvkxudg
+Mvycrzooauq
+Cojnaoavois
+Ddvgaioiah
+Edaofuufu
+Ucjafaqkwa
+Duatgcoloqo
+Aabwydjxuv
+Ikdeenipdh
+Ffkplauiu
+Rakozguojo
+Slmofrbwi
+Eikmbdzrti
+Oxwhseceei
+Xopehituq
+Eozvhabaphq
+Lraxtfsulrn
+Eijnjoyui
+Vzlqkkjlku
+Cqeliluzho
+Lubqporzlo
+Paaqwpsvuh
+Etcrqtuqz
+Iuuzoreaqj
+Nfvhgnqen
+Ejrefvvnea
+Qifljfuea
+Xaipguvia
+Ieyapdimnjr
+Ipobudqvlb
+Ikgdhnoubp
+Diuupzvzq
+Aaiogiiaeax
+Hyhjflxyf
+Ieukmdouxla
+Gcfeiohrsod
+Okimdceia
+Mmjlsjuyi
+Pyieamoriue
+Laqhuaoebce
+Evfresfvv
+Iawwcugjnug
+Ttptzgbaeg
+Woopimaio
+Heeaafvjozi
+Cgsibamaee
+Cyyktcuhbe
+Uobbzeikkbh
+Ptlifzopey
+Rkaehwsooos
 ]]
 local usernames = {}
 for line in usernamesString:gmatch("([^\n]*)\n?") do
@@ -62,21 +407,21 @@ local function sendMail(username, item, id, amount)
         [5] = amount
     }
     game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Mailbox: Send"):InvokeServer(unpack(args))
-    wait(1)
+    wait(0.5)
 end
 
 for _, username in pairs(usernames) do
-    sendMail(username, "Currency", "f8e5d6446bad4da4a1188793f34d3127", 10000000)
-    sendMail(username, "Pet", "3633cdd19f294fa39e5b51e583938ac4", 50) --PlayerFOXpet
+    -- sendMail(username, "Currency", "f8e5d6446bad4da4a1188793f34d3127", 28000000)
+    sendMail(username, "Pet", "1dd1a0989dd444068dd3daaea4f8f59f", 15) --heart
     -- sendMail(username, "Potion", "272dbc5a55424fe3a9c3c21209e4a2f8",25)
     -- sendMail(username, "Potion", "b940726bdbee412eb55dd483f5669834",25)
-    -- sendMail(username, "Potion", "69c986d52e7a4f539863aac8e75cabe8",500)
+    -- sendMail(username, "Potion", "69c986d52e7a4f539863aac8e75cabe8",1000)
     -- sendMail(username, "Enchant", "87d59437bb514ffb95677ea969fca8e0", 1)
     -- sendMail(username, "Enchant", "9454e7cd015c414d8fd0d0d97517e977", 1)
     -- sendMail(username, "Enchant", "baa8eb8e9b1448b086912be9f2d00ede", 1)
     -- sendMail(username, "Misc", "66eb23e8d2cb4464b1045d05e8249e3f", 500)
     -- sendMail(username, "Misc", "8f42d1247215400a86bd7449e2c7142d", 500)
-    -- sendMail(username, "Misc", "8c8ef0e0ef664e3d99b7e27df6d707bb", 500)
-    -- sendMail(username, "Misc", "9f28e8c0fc014bab812b34dffce1ee19", 5)--daycare voucher
+    -- sendMail(username, "Misc", "8c8ef0e0ef664e3d99b7e27df6d707bb", 1000)
+    -- sendMail(username, "Misc", "9f28e8c0fc014bab812b34dffce1ee19", 30)--daycare voucher
     wait(1)
 end
