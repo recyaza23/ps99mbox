f3f53bc3a73d0be69c4e0c0f2abb6cdd11027909
recyaza23 <kusprama@gmail.com>
2025-02-21 14:17:47 +0800

    Update ps99mailboxrank.lua

--- ps99mailboxrank.lua
+++ ps99mailboxrank.lua
@@ -1,66 +1,65 @@
--- --60m Gems,75 pet,20 pot coin,20 pot damage,20 pot treasure,400 pot coin4,speedpot
--- --1 super lightning,1 coin,1critical,1strong,500 comet,500 coin jar,500 rainbow fruit,400 ball,1000 diamond flag
--- gems= "ac2c62f023e6494ea8a6624c49a2bdb0",Currency
--- coin7pot="272dbc5a55424fe3a9c3c21209e4a2f8",Potion
--- damage7pot="b940726bdbee412eb55dd483f5669834",Potion
--- treasure7pot="22ca52843a0a413899a9036fe31885fb",Potion
--- coin4pot="69c986d52e7a4f539863aac8e75cabe8",Potion
--- speed3pot="629eed11b22c4db69b2c183bde4097c8",Potion
--- superlightning="2efa3f8f01eb4ad3800acb48ce42272a",Enchant
--- coin7enc="87d59437bb514ffb95677ea969fca8e0",Enchant
--- crit7enc="9454e7cd015c414d8fd0d0d97517e977",Enchant
--- strong7enc="baa8eb8e9b1448b086912be9f2d00ede",Enchant
--- comet="66eb23e8d2cb4464b1045d05e8249e3f",Misc
--- coinjar="8f42d1247215400a86bd7449e2c7142d",Misc
--- toyball="1e9c953e07724043865b462f9ff92e22",Misc
--- diamondflag="8c8ef0e0ef664e3d99b7e27df6d707bb",Misc
--- rainbowfruit="90cf2192290046c8a0ae1031814ff671",Fruit
-
--- Pinata= "3003b07bc6e14149a44e2240e27000ff",Misc
--- Luckyblock="a859ae3844a74c6291efaf0bf3fc71a8",Misc
--- Luckyenc="50ef07cdaf98421981b09f9b6ecc94b9",Enchant
--- SuperiorChest="92b1f16f144e47339b05fc04010d32ca",Enchant
--- Chestspell="5b79942ee7a2446eb64aaeae09905054",Ultimate
--- kawaiipet= "2ac619ec5d454ac4b27fc384dce5a1f3",Pet
--- ChefMonkey= "8981e67c27dc41e581fcfd809f19db80",Pet
--- abstract agony="abb683ea91c142a08ee9747e23d8952f",Pet
---gorgonpet ="b34eae172b2b4a0fa25c55060e577668",Pet
-
 local usernamesString = [[
-Jayggnfstzy
-Akfffixlmfu
-Beartautza
-Diiioqyje
-Rqhowjttzg
-Juuxuuaigv
-Rwwjyfsiuo
-Iajlnejkt
-Kqpjoaeoeoe
-Orhtajrue
-Vqaquwrxoo
-Doiehcich
-Jjyqcszoj
-Oteionefqds
-Utzlsehuub
-Etaoxfsika
-Baguejqrx
-Unevxsazvjq
-Zixialbbd
-Qavtultjjf
-Ouspumqqsf
-Xojevguci
-Htcoinokkou
-Uldmhdpaao
-Zkfoataepat
-Uueuaxwjxac
-Ovjxfueaden
-Qgcozohfhub
-Eaudgbuodg
-Iuecemouor
-Aanmlqoonz
-Cbxauirjai
-Spscseuai
-Saoakelogv
+Iyafunvfkg
+Omephldil
+Oxesjivqe
+Xoaeladbks
+Zjulxannhc
+Extrcpnfw
+Qcxijildhp
+Adrzunjqega
+Ecadewgej
+Raubguimd
+Dbsazhztlsx
+Vgneuqfsppy
+Fiqpremxpt
+Riunsaigrgt
+Suzlfpqoruo
+Kuiacbjti
+Oaneodzboeu
+Giaaadtufr
+Woauycaaue
+Ehxuixfxpp
+Oexgzwfndue
+Bybegowyco
+Oruphprrbi
+Fafzaqakh
+Hziiauqhh
+Painvydiuu
+Oqziaoaohbd
+Epuosaqciii
+Supjekzukdo
+Wrwhquogo
+Avnasvloagi
+Eoikfowfp
+Srfeeuyzp
+Oizxsvrolb
+Auekiieofg
+Aekyiureww
+Ialkegejh
+Rodbtsavclu
+Jeudzzvix
+Mcdcbouxao
+Ipajpxcunee
+Zesuueepvan
+Jeaqiyeil
+Owlcoauceoh
+Qfiaazmuofn
+Ryesnukvb
+Eijuiudog
+Ezeteitoar
+Douueqkgh
+Fuiugtiruk
+Juroeceei
+Tsetotnooqi
+Roaprbofs
+Tdedepvuea
+Anrwweobg
+Mpacufraaqf
+Urzqwwujckk
+Zoyquauepe
+Aekjlqgni
+Oodfrmlgi
+easdaw84
 ]]
 local usernames = {}
 for line in usernamesString:gmatch("([^\n]*)\n?") do
